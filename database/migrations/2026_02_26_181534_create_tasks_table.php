9bbc3867b9a046f6285dd17c9f625b70ac0ab967
husnainisworking <husnainisworking@gmail.com>
2026-02-27 00:02:38 +0500

    feat: add Category and Task models with migrations

---
+++ database/migrations/2026_02_26_181534_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('category_id')->nullable()->constrained()->nullOnDelete();
+            $table->boolean('is_recurring')->default(0);
+            $table->dateTime('completed_at')->nullable();
+            $table->dateTime('task_date')->nullable();
+            $table->timestamps();
+
+            $table->index('user_id');
+            $table->index('category_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
